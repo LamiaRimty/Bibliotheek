63cf3de52cb7b08bf14673112a767bf96755b458
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-19 17:16:29 +0100

    nodejs mysql installation

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express"
+import mysql from "mysql"
 
 const app= express()
 
-const db = mysql.createconnection({
+const db = mysql.createConnection({
     host: "localhost",
     user:"root",
-    password:"",
+    password:"password",
     database:"testbook"
+})
+
+app.get("/",(req,res)=>{
+    res.json("This is the bibliotheek's bookbackend")
 })
 
 app.listen(8800,()=>{
