cd4d7f52e58d630e5449a050bc11080c5c39fe8c
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-20 19:10:34 +0100

    fatch data from mysql

--- rimty-book/src/pages/Books/Books.jsx
+++ rimty-book/src/pages/Books/Books.jsx
@@ -1,8 +1,40 @@
-import React from "react";
 import "./Books.css";
+import { useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+//import { Link } from "react-router-dom";
 
 const Books = () => {
-  return <div>Books</div>;
+  const [books, setbooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setbooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Rimty Bibliotheek📚</h1>
+      <div className=" books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <h4>{book.author}</h4>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Books;
