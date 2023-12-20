ec9447c85aa2fae2689920a05678e579d0447855
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-20 11:56:30 +0100

    create a new book data using postman

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,44 @@ const app= express()
 const db = mysql.createConnection({
     host: "localhost",
     user:"root",
-    password:"password",
+    password:"bibliotheek123",
     database:"testbook"
 })
+
+//if there is an auth problem 
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY "bibliotheek123";
+
 
 app.get("/",(req,res)=>{
     res.json("This is the bibliotheek's bookbackend")
 })
+
+//getting all books from db
+
+app.get("/books",(req,res)=>{
+  const q= "SELECT * FROM testbook.books";
+  db.query(q,(err,data)=>{
+    if(err) return res.json(err)
+    return res.json(data)
+  })
+})
+
+//create a new book
+//INSERT INTO table_name(column_1,column_2,column_3) VALUES (value_1,value_2,value_3);
+app.post("/books",(req,res)=>{
+    const q = "INSERT INTO books(`title`,`desc`,`cover`) VALUES (?)";
+    const values=[
+        "title from bookbackend",
+        "desc from bookbackend",
+        "cover from bookbackend",
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Wow,Book has been created!")
+      })
+})
+
 
 app.listen(8800,()=>{
     console.log("Connected to bookbackend!")
