90bfb3828948c8de51dfa432e21fa6399129f702
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-12-20 16:22:21 +0100

    added react router

--- rimty-book/src/App.js
+++ rimty-book/src/App.js
@@ -1,11 +1,28 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    
+    <Router>
+      <div className="App">
+        <Header />
 
-      </header>
-    </div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route exact path="/details/:id" element={<Details />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/compose" element={<Compose />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+ 
   );
 }
 
